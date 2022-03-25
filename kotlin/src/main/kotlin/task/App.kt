f7b7ca4e0cfe4ec2284d41965180caa79021bc38
eRkec69 <93291109+eRkec69@users.noreply.github.com>
2022-03-25 12:38:16 +0100

    test

--- kotlin/src/main/kotlin/task/App.kt
+++ kotlin/src/main/kotlin/task/App.kt
@@ -4,18 +4,64 @@ import java.io.File
 import java.io.InputStream
 import java.util.LinkedList
 
-fun echo(stream: InputStream) {
-    val buffer = LinkedList<Byte>()
+data class Token(val value: Int, val lexeme: String, val startRow: Int, val startColumn: Int)
 
-    while (true) {
-        val symbol = stream.read()
-        if (symbol == -1) break
-        buffer.add(symbol.toByte())
+interface Scanner {
+    fun eof(): Boolean
+    fun getToken(): Token?
+}
 
+class MockupScanner(private var tokens: List<Token>) : Scanner {
+    override fun eof() = tokens.isEmpty()
+
+    override fun getToken() =
+        if (tokens.isEmpty()) null
+        else {
+            val token = tokens.first()
+            tokens = tokens.drop(1)
+            token
+        }
+}
+
+class Rezognizer(private val scanner: Scanner) {
+    private var last: Token? = null
+
+    fun recognize(): Boolean {
+        last = scanner.getToken()
+        val status = recognizeE()
+        return if (last == null) status
+        else false
     }
-    println(String(buffer.toByteArray()))
+
+    fun recognizeE() = recognizeT() && recognizeE_()
+
+    fun recognizeE_() {
+        val lookahead = last?.value
+        if (lookahead == null) {
+            true
+        }
+        return when(lookahead) {
+            PLUS -> recognizeTerminal(PLUS) && recognizeE()
+            MINUS -> recognizeTerminal(MINUS) && recognizeE()
+            RPAREN -> true
+            else -> false
+        }
+    }
+
+    // ...
+
+    private fun recognizeTerminal(value: Int) =
+        if (last?.value == value) {
+            last = scanner.getToken()
+            true
+        }
+        else false
 }
 
 fun main(args: Array<String>) {
-    echo(File(args[0]).inputStream())
+    if (Rezognizer(MockupScanner(listOf(Token(1, "1.0", 1, 1)))).recognize()) {
+        print("accept")
+    } else {
+        print("reject")
+    }
 }
