320d6420def3cfc9e3d7eab88450ad8f25477d13
eRkec69 <93291109+eRkec69@users.noreply.github.com>
2022-03-25 13:58:16 +0100

    test9

--- kotlin/src/main/kotlin/task/App.kt
+++ kotlin/src/main/kotlin/task/App.kt
@@ -178,7 +178,66 @@ fun printTokens(scanner: Scanner) {
     }
 }
 
+data class Token(val value: Int, val lexeme: String, val startRow: Int, val startColumn: Int)
+
+interface Scanner {
+    fun eof(): Boolean
+    fun getToken(): Token?
+}
+
+class MockupScanner(private var tokens: List<Token>) : Scanner {
+    override fun eof() = tokens.isEmpty()
+
+    override fun getToken() =
+        if (tokens.isEmpty()) null
+        else {
+            val token = tokens.first()
+            tokens = tokens.drop(1)
+            token
+        }
+}
+
+class Rezognizer(private val scanner: Scanner) {
+    private var last: Token? = null
+
+    fun recognize(): Boolean {
+        last = scanner.getToken()
+        val status = recognizeE()
+        return if (last == null) status
+        else false
+    }
+
+    fun recognizeE() = recognizeT() && recognizeE_()
+
+    fun recognizeE_() {
+        val lookahead = last?.value
+        if (lookahead == null) {
+            true
+        }
+        return when(lookahead) {
+            PLUS -> recognizeTerminal(PLUS) && recognizeE()
+            MINUS -> recognizeTerminal(MINUS) && recognizeE()
+            RPAREN -> true
+            else -> false
+        }
+    }
+
+    // ...
+
+    private fun recognizeTerminal(value: Int) =
+        if (last?.value == value) {
+            last = scanner.getToken()
+            true
+        }
+        else false
+}
+
 fun main(args: Array<String>) {
     val scanner = Scanner(Example, File(args[0]).inputStream())
     printTokens(scanner)
+    if (Rezognizer(MockupScanner(listOf(Token(1, "1.0", 1, 1)))).recognize()) {
+        print("accept")
+    } else {
+        print("reject")
+    }
 }
